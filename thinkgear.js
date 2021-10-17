ab0684868f2f81b1aa78878969fda931d1c37cb8
Szymon Skarzyński <szymon3301@gmail.com>
2021-10-17 17:43:14 +0200

    add blink_data and raw_data receiving and logging

--- thinkgear.js
+++ thinkgear.js
@@ -4,8 +4,21 @@ const client = createClient({
     enableRawOutput: true,
 });
 
-client.on('data', data => {
-    console.log(data);
-});
+client
+    .on('data', data => {
+        console.log('Data start');
+        console.log(data);
+        console.log('Data stop');
+    })
+    .on('blink_data', data => {
+        console.log('Blink data start');
+        console.log(data);
+        console.log('Blink data stop');
+    })
+    .on('raw_data', data => {
+        console.log('Raw data start');
+        console.log(data);
+        console.log('Raw data stop');
+    });
 
 client.connect();
