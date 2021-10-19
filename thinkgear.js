f01c5272a25ea2e2f9ed15a3c1c34abbebfecc8c
Szymon Skarzyński <szymon3301@gmail.com>
2021-10-20 00:05:39 +0200

    add creating json files with data

--- thinkgear.js
+++ thinkgear.js
@@ -1,24 +1,84 @@
 const {createClient} = require('node-thinkgear-sockets');
+const prompts = require('prompts');
+const {writeFile, readdir, mkdir} = require('fs').promises;
 
-const client = createClient({
-    enableRawOutput: true,
-});
+const jsonTemplate = {
+    eSense: {
+        attention: [],
+        meditation: [],
+    },
+    eegPower: {
+        delta: [],
+        theta: [],
+        lowAlpha: [],
+        highAlpha: [],
+        lowBeta: [],
+        highBeta: [],
+        lowGamma: [],
+        highGamma: [],
+    }
+}
 
-client
-    .on('data', data => {
-        console.log('Data start');
-        console.log(data);
-        console.log('Data stop');
-    })
-    .on('blink_data', data => {
-        console.log('Blink data start');
-        console.log(data);
-        console.log('Blink data stop');
-    })
-    .on('raw_data', data => {
-        console.log('Raw data start');
-        console.log(data);
-        console.log('Raw data stop');
-    });
+const blinkData = [];
 
-client.connect();
+const questions = [
+    {
+        type: 'text',
+        name: 'subject',
+        message: 'Enter subject name/ID',
+    },
+    {
+        type: 'text',
+        name: 'experiment',
+        message: 'Enter experiment name',
+    }
+];
+
+(async () => {
+
+    const {subject, experiment} = await prompts(questions);
+    try {
+        await readdir(`data/${subject}`);
+    } catch (e) {
+        await mkdir(`data/${subject}`);
+    }
+    const dataFileName = `data/${subject}/${experiment}.json`;
+    const blinkFileName = `data/${subject}/${experiment}_blink.json`;
+
+    const client = createClient();
+
+    client
+        .on('data', async data => {
+            with (jsonTemplate.eSense) {
+                attention.push(data.eSense.attention);
+                meditation.push(data.eSense.meditation);
+            }
+            with (jsonTemplate.eegPower) {
+                delta.push(data.eegPower.delta);
+                theta.push(data.eegPower.theta);
+                lowAlpha.push(data.eegPower.lowAlpha);
+                highAlpha.push(data.eegPower.highAlpha);
+                lowBeta.push(data.eegPower.lowBeta);
+                highBeta.push(data.eegPower.highBeta);
+                lowGamma.push(data.eegPower.lowGamma);
+                highGamma.push(data.eegPower.highGamma);
+            }
+            try {
+                await writeFile(dataFileName, jsonTemplate, 'utf-8');
+            } catch (e) {
+                console.error(e)
+            }
+        })
+        .on('blink_data', async data => {
+            blinkData.push(data.blinkStrength);
+            try {
+                await writeFile(blinkFileName, blinkData, 'utf-8');
+            } catch(e) {
+                console.error(e);
+            }
+        });
+
+    client.connect();
+
+})();
+
