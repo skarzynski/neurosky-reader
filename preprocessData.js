75951bf0b39511c500a12feda95a9a03950365c3
Szymon Skarzyński <szymon3301@gmail.com>
2021-12-02 21:02:11 +0100

    fix preprocessing

--- preprocessData.js
+++ preprocessData.js
@@ -4,18 +4,28 @@ const preprocess = async (subject, experiment) => {
 
     const data = JSON.parse(await readFile(`data/${subject}/${experiment}.json`));
     const {eegPower} = data;
+    const eegCopy = {
+        delta: [...eegPower.delta],
+        theta: [...eegPower.theta],
+        lowAlpha: [...eegPower.lowAlpha],
+        highAlpha: [...eegPower.highAlpha],
+        lowBeta: [...eegPower.lowBeta],
+        highBeta: [...eegPower.highBeta],
+        lowGamma: [...eegPower.lowGamma],
+        highGamma: [...eegPower.highGamma],
+    }
 
     for (let i = 0; i < eegPower.delta.length; i++) {
         if (i > 9 && i < eegPower.delta.length - 10) {
 
-            eegPower.delta[i] = eegPower.delta.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16;
-            eegPower.theta[i] = eegPower.theta.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16;
-            eegPower.lowAlpha[i] = eegPower.lowAlpha.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16;
-            eegPower.highAlpha[i] = eegPower.highAlpha.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16;
-            eegPower.lowBeta[i] = eegPower.lowBeta.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16;
-            eegPower.highBeta[i] = eegPower.highBeta.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16;
-            eegPower.lowGamma[i] = eegPower.lowGamma.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16;
-            eegPower.highGamma[i] = eegPower.highGamma.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16;
+            eegPower.delta[i] = Math.round(eegCopy.delta.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16);
+            eegPower.theta[i] = Math.round(eegCopy.theta.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16);
+            eegPower.lowAlpha[i] = Math.round(eegCopy.lowAlpha.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16);
+            eegPower.highAlpha[i] = Math.round(eegCopy.highAlpha.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16);
+            eegPower.lowBeta[i] = Math.round(eegCopy.lowBeta.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16);
+            eegPower.highBeta[i] = Math.round(eegCopy.highBeta.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16);
+            eegPower.lowGamma[i] = Math.round(eegCopy.lowGamma.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16);
+            eegPower.highGamma[i] = Math.round(eegCopy.highGamma.slice(i - 8, i + 8).reduce((a, b) => a + b) / 16);
 
         }
     }
@@ -29,7 +39,6 @@ const preprocess = async (subject, experiment) => {
     eegPower.lowGamma = eegPower.lowGamma.slice(10, eegPower.lowGamma.length - 10);
     eegPower.highGamma = eegPower.highGamma.slice(10, eegPower.highGamma.length - 10);
 
-    data.eegPower = eegPower;
     try {
         await writeFile(`data/${subject}/${experiment}_preprocessed.json`, JSON.stringify(data), 'utf-8');
         console.log('Preprocessed');
